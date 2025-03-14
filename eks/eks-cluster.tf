635cde2d1127c3a058990478f47c24544cc45845
Michael Pulido <mike@nethopper.io>
2025-03-14 09:25:10 -0400

    updating all versions

--- eks/eks-cluster.tf
+++ eks/eks-cluster.tf
@@ -1,6 +1,6 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 20.31"
+  version = "~> 20.34.0"
 
   cluster_name    = local.cluster_name
   cluster_version = var.k8s-version 
--- eks/versions.tf
+++ eks/versions.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.15.0"
+      version = "~> 5.91.0"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "3.1.0"
+      version = "3.7.1"
     }
   }
 
-  required_version = "~> 1.11.2"
+  required_version = "~> 1.2.0"
 }
